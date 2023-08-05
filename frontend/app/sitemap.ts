b56591a1f4419cee240ddeecb48c54e2a7080585
Jared Truscott <jtruscott78@gmail.com>
2023-08-05 15:55:48 -0400

    tweak seo

---
+++ frontend/app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://jaredmakes.com',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://jaredmakes.com/projects',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://jaredmakes.com/blog',
+      lastModified: new Date(),
+    },
+  ]
+}
